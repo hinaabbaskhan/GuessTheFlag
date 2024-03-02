bc4c1ce57d45be78555a0ea6e2c9b0fc9deaacf4
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-02 14:24:47 +0500

    spacer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(spacing: 20) {
+        VStack() {
+            Spacer()
             Text("Hello, world!")
             Text("This is inside a stack")
+            Spacer()
         }
         .padding()
     }
