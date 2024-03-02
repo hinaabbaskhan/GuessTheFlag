5a4249e5288eede5312afd0347b53f22e1160282
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-02 14:40:51 +0500

    add color primary

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack() {
-            Color.red
+            Color.primary
                 .frame(minWidth:200, maxWidth:.infinity,  maxHeight: 200)
             Text("Your Content")
         }
+        
     }
 }
 
