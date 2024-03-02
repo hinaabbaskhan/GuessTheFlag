7b9fc5a6e429038e89197f30ebb6d61893afaa50
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-02 14:39:18 +0500

    add color and frames

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack() {
+            Color.red
+                .frame(minWidth:200, maxWidth:.infinity,  maxHeight: 200)
             Text("Your Content")
         }
-        .background(.red)
     }
 }
 
