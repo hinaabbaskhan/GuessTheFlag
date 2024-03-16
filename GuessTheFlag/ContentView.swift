d651a58771adc3c6a8afdb9dc9e063d55aff0d40
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-16 16:57:15 +0500

    rename content view

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GuessTheFlagContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var scoreTitle=""
